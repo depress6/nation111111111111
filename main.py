90fea1809dfc113b013cfd8f7b5002af1d05192e
superman3000-sys <farfey123@mail.ru>
2023-04-04 18:31:13 +0300

    new function ''

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+# This is a sample Python script.
 
-import telebot
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-bot = telebot.TeleBot("5698088306:AAEvKMetal9oAzISOHqt4MYbn13A5tpfdhU")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
 
-bot.polling()
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
